459a31005456e1a8e1543f39296bbcad017be5e0
Morgan McDonald <morganmcdee13@outlook.com>
2024-09-15 12:01:08 +0100

    Global State

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, voluptatibus asperiores cupiditate reprehenderit earum natus consequuntur amet sapiente accusamus aut.',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, voluptatibus asperiores cupiditate reprehenderit earum natus consequuntur amet sapiente accusamus aut.',
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, voluptatibus asperiores cupiditate reprehenderit earum natus consequuntur amet sapiente accusamus aut.',
+  },
+];
+
+export default FeedbackData;
